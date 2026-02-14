031afc13ac21cc2d5ab2080b9f31dd6178f2b5b9
silex <46520333+silex-rr@users.noreply.github.com>
2026-02-14 07:41:53 +0400

    RepositoryInterface fixed

--- src/Core/Application/Event/EventHandlerInterface.php
+++ src/Core/Application/Event/EventHandlerInterface.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace Inquisition\Core\Application\Event;
 
+/**
+ * @template T of EventInterface
+ */
 interface EventHandlerInterface
 {
     /**
      * Handle the domain event
+     *
+     * @param T $event
      */
     public function handle(EventInterface $event): void;
 
     /**
      * Get the event types this handler can process
-     * @return string[]
+     * @return array<class-string<T>>
      */
     public function getHandledEvents(): array;
 
